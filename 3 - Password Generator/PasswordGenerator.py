38f6f8421d830a81001af7fbc37dfbaf0cc80b93
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-05-04 16:56:00 -0400

    add comprehension comments

--- 3 - Password Generator/PasswordGenerator.py
+++ 3 - Password Generator/PasswordGenerator.py
@@ -2,7 +2,7 @@ import random
 import string
 
 def generate_password(min_length, numbers=True, special_characters=True):
-# Get letts, digits, special characters
+# Get letters, digits, and special characters to choose from when generating a password
 
     letters = string.ascii_letters
     digits = string.digits
@@ -12,11 +12,11 @@ def generate_password(min_length, numbers=True, special_characters=True):
 
     characters = letters
     if numbers:
-        characters += digits
+        characters += digits # if numbers = True, add them to the password(characters variable)
     if special_characters:
-        characters += special
+        characters += special # if special characters = True, add them to the password(characters variable)
 
-# No password, criteria met, numbers, or special characters
+# Baseline no password, criteria met, numbers, or special characters
 
     pwd = ""
     meets_criteria = False
@@ -26,7 +26,7 @@ def generate_password(min_length, numbers=True, special_characters=True):
 # Generate new character if it doesn't meet criteria and is too short
 
     while not meets_criteria or len(pwd) < min_length:
-        new_char = random.choice(characters)
+        new_char = random.choice(characters) # randomly picks a character from the characters variable
         pwd += new_char
 
 # Determine if a number
@@ -38,9 +38,9 @@ def generate_password(min_length, numbers=True, special_characters=True):
 
         meets_criteria = True
         if numbers:
-            meets_criteria = has_number
+            meets_criteria = has_number # if numbers included, meets_criteria will be set to True
         if special_characters:
-            meets_criteria = meets_criteria and has_special
+            meets_criteria = meets_criteria and has_special # if special included, meets_criteria is set to True
 
     return pwd
 
