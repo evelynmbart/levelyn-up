f57c93e0a0d23e59754ebffb32f3d61c7120cfb5
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-07-24 11:54:09 -0400

    install phosphor icons

--- 31 - Star rating (react app/starrating/src/App.js
+++ 31 - Star rating (react app/starrating/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { Star } from 'phosphor-react';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
+      <Star size={32} />
     </div>
   );
-}
+};
 
-export default App;
+// create stars (either dark or transparent)
+// when hovered, change their background color to gold
+// when clicked, change their background color to gold 
+// use state to remember the number of stars clicked
