eddcf184e0748fc651aafacbea629ac323349ebc
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-07-24 14:18:59 -0400

    star rating hover

--- 31 - Star rating (react app/starrating/src/App.js
+++ 31 - Star rating (react app/starrating/src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
-import { Star } from 'phosphor-react';
+import { Star } from "@phosphor-icons/react/dist/ssr/Star";
+import { useState } from 'react';
+
+
+function Rating({ hoveredStar, mouseLeave, mouseEnter, starNumber }) {
+  return (
+    <>
+      <Star className="star" size={70} color={"gold"} weight={hoveredStar && hoveredStar >= starNumber ? "fill" : undefined} onMouseEnter={() => mouseEnter(starNumber)} onMouseLeave={mouseLeave} />
+    </>
+  )
+}
 
 export default function App() {
+  const [hoveredStar, setHoveredStar] = useState(null);
+  const [filledStars, setFilledStars] = useState(false);
+  function mouseEnter(starNumber) {
+    setHoveredStar(starNumber)
+  }
+  function mouseLeave() {
+    setHoveredStar(null)
+  }
+
+
   return (
     <div className="App">
-      <Star size={32} />
+      <Rating hoveredStar={hoveredStar} mouseLeave={mouseLeave} mouseEnter={mouseEnter} starNumber={1} />
+      <Rating hoveredStar={hoveredStar} mouseLeave={mouseLeave} mouseEnter={mouseEnter} starNumber={2} />
+      <Rating hoveredStar={hoveredStar} mouseLeave={mouseLeave} mouseEnter={mouseEnter} starNumber={3} />
+      <Rating hoveredStar={hoveredStar} mouseLeave={mouseLeave} mouseEnter={mouseEnter} starNumber={4} />
+      <Rating hoveredStar={hoveredStar} mouseLeave={mouseLeave} mouseEnter={mouseEnter} starNumber={5} />
     </div>
   );
 };
