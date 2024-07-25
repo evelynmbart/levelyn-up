5cf8847438329be142345fa3b880ed812935722c
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-07-25 19:17:04 -0400

    single and multi select accordion menu

--- 29 - Accordion Menu (react app)/accordionmenuvideo/src/App.js
+++ 29 - Accordion Menu (react app)/accordionmenuvideo/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Accordian from './components';
 
 function App() {
   return (
     <div className="App">
+      <Accordian />
     </div>
   );
 }
---
+++ 29 - Accordion Menu (react app)/accordionmenuvideo/src/components/data.js
@@ -0,0 +1,23 @@
+const data = [
+    {
+        id: 1,
+        question: "Dogs?",
+        answer: "Dogs are cute as can be"
+    },
+    {
+        id: 2,
+        question: "Cats?",
+        answer: "Cats are, believe it or not, CUTER"
+    },
+    {
+        id: 3,
+        question: "Ducks?",
+        answer: "I only really like runner ducks, but ducks are pretty cute overall"
+    },
+    {
+        id: 4,
+        question: "Turtles?",
+        answer: "I don't know much about turtles, wouldn't want one. But they cute as a living creature"
+    }
+]
+export default data;
