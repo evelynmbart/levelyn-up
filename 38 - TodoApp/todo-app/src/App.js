86e1b4f5ea1b0c5a5eea850af8f19c54e924e396
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-07-31 18:05:45 -0400

    the rest

--- 38 - TodoApp/todo-app/src/App.js
+++ 38 - TodoApp/todo-app/src/App.js
@@ -5,23 +5,44 @@ import { useState } from 'react';
 
 function App() {
   const [task, setTask] = useState([]);
-  const [userInput, setUserInput] = useState('');
+  const [userInputValue, setUserInputValue] = useState('');
 
   const handleUserInputChange = (e) => {
-    setUserInput(e.target.value);
+    setUserInputValue(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('iwasclicked')
+    setTask([userInputValue, ...task]);
+    setUserInputValue('');
+  }
+
+  const handleDelete = (i) => {
+    const newTasks = [...task]
+    newTasks.splice(i, 1)
+    setTask(newTasks)
   }
 
   return (
     <div>
       <div className="navbar">
         Today's TO-DO's:
-        <input className="userInput" type="text" placeholder="Laundry" />
-        <button type='submit' className='submitButton'>Submit</button>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="userInput"
+            type="text"
+            value={userInputValue}
+            onChange={handleUserInputChange}
+            placeholder="Laundry">
+          </input>
+          <button type="submit" className='submitButton'>Add</button>
+        </form>
       </div>
       <div className='board'>
-        {/* {task.map((task) => {
-          return <TodoItem item={task} />
-        })} */}
+        {task.map((task, index) => {
+          return <TodoItem key={index} item={task} handleDelete={() => handleDelete(index)} />
+        })}
       </div>
     </div>
   );
--- 38 - TodoApp/todo-app/src/components/TodoItem/TodoItem.jsx
+++ 38 - TodoApp/todo-app/src/components/TodoItem/TodoItem.jsx
@@ -1,10 +1,29 @@
-export function TodoItem() {
+import { useState } from "react";
+
+export function TodoItem({ item, handleDelete }) {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleChange = (e) => {
+    setIsChecked(e.target.checked);
+  };
+
   return (
     <div className="item">
-      <input type="checkbox" className="checkbox" />{" "}
-      {/* when clicked, strikethrough the item */}
-      <h2 className="title"></h2>
-      <button className="deleteButton">Delete</button>
+      <input
+        type="checkbox"
+        className="checkbox"
+        value={isChecked}
+        onChange={handleChange}
+      />
+      <h2
+        className="title"
+        style={{ textDecoration: isChecked ? "line-through" : "none" }}
+      >
+        {item}
+      </h2>
+      <button className="deleteButton" onClick={handleDelete}>
+        Delete
+      </button>
     </div>
   );
 }
