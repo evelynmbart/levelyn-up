64811301d989f69e2cd4d022e9e0164af2bf3e3e
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-08-20 17:00:10 -0400

    memory react game!

--- 42 - Memory/memory-game/src/App.js
+++ 42 - Memory/memory-game/src/App.js
@@ -1,19 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import './components/Card.css'
 import { Card } from './components/Card';
 
 const cardImages = [
-  { src: '/img/blueberry.png' },
-  { src: '/img/cinnamon.png' },
-  { src: '/img/chocolate.png' },
-  { src: '/img/lemon.png' },
-  { src: '/img/strawberry.png' },
-  { src: '/img/vanilla.png' }
+  { src: '/img/blueberry.png', matched: false },
+  { src: '/img/cinnamon.png', matched: false },
+  { src: '/img/chocolate.png', matched: false },
+  { src: '/img/lemon.png', matched: false },
+  { src: '/img/strawberry.png', matched: false },
+  { src: '/img/vanilla.png', matched: false }
 ]
 
 export default function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null); // these update the user's choice of card. 
+  const [choiceTwo, setChoiceTwo] = useState(null); //first is set to choiceOne, second pick is set to choiceTwo
+  const [disabled, setDisabled] = useState(false);
+
 
   //shuffle cards
   // 1. duplicate each card once
@@ -24,11 +29,52 @@ export default function App() {
       .sort(() => Math.random() - 0.5) // shuffles cards
       .map((card) => ({ ...card, id: Math.random() })) // gives a random Id
 
+    setChoiceOne(null)
+    setChoiceTwo(null)
     setCards(shuffledCards) // shuffle cards
     setTurns(0); // setting turns to 0
   }
-  console.log(cards, turns);
 
+  // handle a choice
+  const handleChoice = (card) => { // if choiceOne doesn't have a value, setChoiceOne. if choiceOne does have a value, setChoiceTwo!
+/* ternary */ choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  // compare 2 selected cards
+  useEffect(() => {
+
+    if (choiceOne && choiceTwo) { // run the useEffect and do a comparison only when both choices are selected
+      setDisabled(true)
+
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000)
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
+  // reset choices and increase turns
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
+  }
+
+  // start a new game automatically
+  useEffect(() => {
+    shuffleCards()
+  }, [])
 
   return (
     <div className='App'>
@@ -37,7 +83,22 @@ export default function App() {
         <div className='sub-title'>Memory Game</div>
       </div>
       <button className='new-game-btn' onClick={shuffleCards}>New Game</button>
-      <Card cards={cards} />
+      <div className='card-grid'>
+        {/*mapping through each card instead of copying and having messy code. 
+        this will return each card + image of donut and display them on the screen (after css) */}
+        {cards.map((card) => (
+          <Card
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
+        ))}
+      </div>
+      <div className='turns'>
+        Turns: {turns}
+      </div>
     </div>
   )
 }
--- 42 - Memory/memory-game/src/components/Card.jsx
+++ 42 - Memory/memory-game/src/components/Card.jsx
@@ -1,16 +1,23 @@
-export function Card({ cards }) {
+import "./Card.css";
+
+export function Card({ card, handleChoice, flipped, disabled }) {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
+    }
+  };
+
   return (
-    <div className="card-grid">
-      {/*mapping through each card instead of copying and having messy code. 
-        this will return each card + image of donut and display them on the screen (after css) */}
-      {cards.map((card) => (
-        <div className="card" key={card.id}>
-          <div>
-            <img className="front" src={card.src} alt="card-front" />
-            <img className="back" src="/img/cover.png" alt="card-back" />
-          </div>
-        </div>
-      ))}
+    <div className="card">
+      <div className={flipped ? "flipped" : ""}>
+        <img className="front" src={card.src} alt="card front" />
+        <img
+          className="back"
+          src="/img/cover.png"
+          onClick={handleClick}
+          alt="card-back"
+        />
+      </div>
     </div>
   );
 }
