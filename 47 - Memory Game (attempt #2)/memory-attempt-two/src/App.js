3f6cfc568c1b49686bc545de1ff0c772844766ff
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-08-23 14:43:34 -0400

    add matching cards functionality, flipping animation, and disabling more than two flips

--- 47 - Memory Game (attempt #2)/memory-attempt-two/src/App.js
+++ 47 - Memory Game (attempt #2)/memory-attempt-two/src/App.js
@@ -1,20 +1,23 @@
 import './App.css';
-import { useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import './components/Card.css'
 import { Card } from './components/Card';
 
 const cardImages = [
-  { src: '/img/blueberry.png' },
-  { src: '/img/chocolate.png' },
-  { src: '/img/cinnamon.png' },
-  { src: '/img/lemon.png' },
-  { src: '/img/strawberry.png' },
-  { src: '/img/vanilla.png' }
+  { src: '/img/blueberry.png', matched: false },
+  { src: '/img/chocolate.png', matched: false },
+  { src: '/img/purples.png', matched: false },
+  { src: '/img/lemon.png', matched: false },
+  { src: '/img/strawberry.png', matched: false },
+  { src: '/img/vanilla.png', matched: false }
 ]
 
 function App() {
-  const [cards, setCards] = useState([])
-  const [turns, setTurns] = useState(0)
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
 
   //duplicate cards
   const shuffleCards = () => {
@@ -22,8 +25,49 @@ function App() {
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }))
 
+
+
     setCards(shuffledCards)
+    setCards((prev) => {
+      return shuffledCards
+    })
     setTurns(0)
+  }
+  console.log(cards)
+
+  const handleChoice = (card) => { // if choiceOne value is null, set choice one to (chosen) card. if choiceone value isn't null, setChoice TWo to (chosen) card
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+
+  //compare the cards
+  // create a new match property set to false, that when the card.src matches, it changes it to true
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true)
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurns();
+      } else {
+        setTimeout(() => resetTurns(), 1000)
+      }
+    }
+  }, [choiceOne, choiceTwo]) // so it fires on each choice change
+
+  const resetTurns = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    //setTurns(prevTurns => prevTurns + 1)
+    setTurns(turns + 1)
+    setDisabled(false)
   }
 
   return (
@@ -35,9 +79,12 @@ function App() {
       <button className='new-game-btn' onClick={shuffleCards}>New Game</button>
       <div className="card-grid">
         {cards.map((card) => {
-          <Card
+          return <Card
             key={card.id}
             card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
           />
         })}
       </div>
--- 47 - Memory Game (attempt #2)/memory-attempt-two/src/components/Card.js
+++ 47 - Memory Game (attempt #2)/memory-attempt-two/src/components/Card.js
@@ -1,9 +1,16 @@
-export function Card({ card }) {
+export function Card({ card, handleChoice, flipped, disabled }) {
+
+    const handleClick = () => {
+        if (!disabled) {
+            handleChoice(card) // need to pass this card or else handleChoice won't actually have values
+        }
+    }
+
     return (
         <div className="card">
-            <div>
-                <img className='front' src={cards.src} alt='card-front' />
-                <img className='back' src='/img/cover.png' alt='card-back' />
+            <div className={flipped ? "flipped" : ""}>
+                <img className='front' src={card.src} alt='card-front' />
+                <img className='back' src='/img/cover.png' onClick={handleClick} alt='card-back' />
             </div>
 
         </div>
