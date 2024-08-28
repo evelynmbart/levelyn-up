3b1b59c234ccdcb696c22809aa11c9b75a5bb662
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-08-28 09:36:38 -0400

    add inputs and labels with each question/multi-choice option on DOM

--- 48 - QuizApp/Quiz-app/src/App.jsx
+++ 48 - QuizApp/Quiz-app/src/App.jsx
@@ -45,6 +45,7 @@ const questions = [
 ];
 
 function App() {
+  const [question, setQuestion] = useState();
   return (
     <>
       <div className="navbar">Pop Quiz!</div>
@@ -53,7 +54,74 @@ function App() {
         <h3>Question #</h3>
         <form>
           <fieldset>
-            <legend></legend>
+            <legend>{questions[0].question}</legend>
+            <div>
+              <input type="radio" id="answer1" name="answer" value="berlin" />
+              <label for="answer1">Berlin</label>
+              <input type="radio" id="answer2" name="answer" value="madrid" />
+              <label for="answer2">Madrid</label>
+              <input type="radio" id="answer3" name="answer" value="paris" />
+              <label for="answer3">Paris</label>
+              <input type="radio" id="answer4" name="answer" value="rome" />
+              <label for="answer4">Rome</label>
+            </div>
+          </fieldset>
+          <fieldset>
+            <legend>{questions[1].question}</legend>
+            <div>
+              <input type="radio" id="answer1" name="answer" value="earth" />
+              <label for="answer1">Earth</label>
+              <input type="radio" id="answer2" name="answer" value="mars" />
+              <label for="answer2">Mars</label>
+              <input type="radio" id="answer3" name="answer" value="jupiter" />
+              <label for="answer3">Jupiter</label>
+              <input type="radio" id="answer4" name="answer" value="venus" />
+              <label for="answer4">Venus</label>
+            </div>
+          </fieldset>
+          <fieldset>
+            <legend>{questions[2].question}</legend>
+            <div>
+              <input type="radio" id="answer1" name="answer" value="H2" />
+              <label for="answer1">H2</label>
+              <input type="radio" id="answer2" name="answer" value="O2" />
+              <label for="answer2">O2</label>
+              <input type="radio" id="answer3" name="answer" value="CO2" />
+              <label for="answer3">CO2</label>
+              <input type="radio" id="answer4" name="answer" value="H2O" />
+              <label for="answer4">H2O</label>
+            </div>
+          </fieldset>
+          <fieldset>
+            <legend>{questions[3].question}</legend>
+            <div>
+              <input type="radio" id="answer1" name="answer" value="spanish" />
+              <label for="answer1">Spanish</label>
+              <input
+                type="radio"
+                id="answer2"
+                name="answer"
+                value="portuguese"
+              />
+              <label for="answer2">Portuguese</label>
+              <input type="radio" id="answer3" name="answer" value="french" />
+              <label for="answer3">French</label>
+              <input type="radio" id="answer4" name="answer" value="italian" />
+              <label for="answer4">Italian</label>
+            </div>
+          </fieldset>
+          <fieldset>
+            <legend>{questions[4].question}</legend>
+            <div>
+              <input type="radio" id="answer1" name="answer" value="elephant" />
+              <label for="answer1">Elephant</label>
+              <input type="radio" id="answer2" name="answer" value="lion" />
+              <label for="answer2">Lion</label>
+              <input type="radio" id="answer3" name="answer" value="tiger" />
+              <label for="answer3">Tiger</label>
+              <input type="radio" id="answer4" name="answer" value="bear" />
+              <label for="answer4">Bear</label>
+            </div>
           </fieldset>
         </form>
       </div>
