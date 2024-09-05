2a756deab864a3ac1f013a8871eed9523e24c491
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-09-05 12:02:25 -0400

    create title and post inputs add simple css

--- 52 - Blog/blog/src/App.jsx
+++ 52 - Blog/blog/src/App.jsx
@@ -1,7 +1,25 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <nav>Blog</nav>
+      <form>
+        <div className="content-container">
+          <div className="user-input">
+            <label>
+              Blog-post title:
+              <input type="text" placeholder="Day One" />
+            </label>
+            <div className="user-input" id="second-label">
+              <label>Write away...</label>
+              <textarea placeholder="Today was a doozy...." />
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  );
 }
 
 export default App;
