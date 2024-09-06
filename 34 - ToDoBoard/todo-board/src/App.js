3a4cbb8d0d21144527baef0bab95e2759b3959e0
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-09-06 15:19:08 -0400

    implement local storage using useeffect and lazy state initialization, update css

--- 34 - ToDoBoard/todo-board/src/App.js
+++ 34 - ToDoBoard/todo-board/src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Board from './components/Board';
 import Input from './components/Input';
 
 export default function App() {
-  const [taskList, setTaskList] = useState([]);
+  const [taskList, setTaskList] = useState(() => JSON.parse(localStorage.getItem('taskList')) || []);
+
+  useEffect(() => {
+    localStorage.setItem('taskList', JSON.stringify(taskList))
+  }, [taskList])
 
   return (
     <div className="App">
       <h1>To Do Board</h1>
       <Input taskList={taskList} setTaskList={setTaskList} />
-      <div>
+      <div className='list-of-tasks'>
         {taskList.map((task, index) =>
           <Board
             key={index}
