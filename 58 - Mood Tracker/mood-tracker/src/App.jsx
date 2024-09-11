b51c8ef4107d5d90c9d8136d46adbb6f1e01f5f3
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-09-11 16:57:47 -0400

    images saved to public, html css updates

--- 58 - Mood Tracker/mood-tracker/src/App.jsx
+++ 58 - Mood Tracker/mood-tracker/src/App.jsx
@@ -1,7 +1,30 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="container">
+        <button className="content"> How are you today?</button>
+        <div className="faces">
+          <button className="happy">
+            <img src="happy.png" />
+          </button>
+          <button className="meh">
+            <img src="meh.png" />
+          </button>
+          <button className="sad">
+            <img src="sad.png" />
+          </button>
+          <button className="angry">
+            <img src="angry.png" />
+          </button>
+        </div>
+
+        <label>What's on your mind?</label>
+        <textarea placeholder="Today was a doozy..."></textarea>
+      </div>
+    </>
+  );
 }
 
 export default App;
