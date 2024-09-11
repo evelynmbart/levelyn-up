9f50f2e688642bca3dc95f5b2d562b2e0bff168c
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-09-11 12:02:04 -0400

    use state to remember the user input. take its length and create a maxCount function that alerts an error message any time the character count exceeds maxCharacter

--- 56 - Character Counter/character-count/src/App.jsx
+++ 56 - Character Counter/character-count/src/App.jsx
@@ -1,7 +1,41 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [characterCount, setCharacterCount] = useState("");
+
+  const handleChange = (e) => {
+    setCharacterCount(e.target.value);
+  };
+  const maxCharacter = 200;
+
+  const greaterThan = characterCount.length >= maxCharacter;
+
+  const maxCount = () => {
+    const error = "too many characters!";
+    if (greaterThan) {
+      alert(error);
+    }
+  };
+
+  console.log(characterCount);
+  return (
+    <>
+      <div>Character Counter</div>
+      <form>
+        <label>Please write your comments here:</label>
+        <textarea onChange={handleChange}></textarea>
+      </form>
+      <div
+        style={{
+          color: greaterThan ? "red" : "white",
+        }}
+      >
+        {maxCount()}
+        {characterCount.length} / {maxCharacter}
+      </div>
+    </>
+  );
 }
 
 export default App;
