b1f84efa6854bff8e9e2ef58c93ac919ed918c06
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-09-11 10:15:39 -0400

    create react app that displays users' name on submit after user input value is stored

--- 54 - Digital Clock/digital-clock/src/App.jsx
+++ 54 - Digital Clock/digital-clock/src/App.jsx
@@ -1,7 +1,41 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [name, setName] = useState("");
+  const [userInput, setUserInput] = useState("");
+
+  const handleChange = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setName(userInput);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Please input first name:
+          <input
+            type="text"
+            name="name"
+            value={userInput}
+            placeholder="rebecca"
+            onChange={handleChange}
+          />
+        </label>
+        <button>Submit</button>
+        {name && <h1>Hello {name}!</h1>}
+      </form>
+    </>
+  );
 }
 
 export default App;
+
+/*
+  
+*/
