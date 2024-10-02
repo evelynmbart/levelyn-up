51b11be10ab18ff44b5eac7cf144f7878fd690e5
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-10-02 13:51:40 -0400

    create main, footer, sidebar component and import to App.jsx

--- 61 - Nasa App/nasa-app/src/App.jsx
+++ 61 - Nasa App/nasa-app/src/App.jsx
@@ -1,4 +1,7 @@
 import "./App.css";
+import { Main } from "./components/Main";
+import { Footer } from "./components/Footer";
+import { SideBar } from "./components/SideBar";
 
 function App() {
   return <></>;
