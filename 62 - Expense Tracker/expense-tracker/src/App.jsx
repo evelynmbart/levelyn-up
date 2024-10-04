1374af4ab7fb72ebc9adc30d0d917237846a00e4
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-10-04 14:29:03 -0400

    install react-router-dom + make main page html and css

--- 62 - Expense Tracker/expense-tracker/src/App.jsx
+++ 62 - Expense Tracker/expense-tracker/src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="main-div">
+      <nav className="navbar">
+        <h1 className="title">Expense Tracker</h1>
+      </nav>
+      <div className="welcome-content">
+        <div className="welcome-div">
+          <div className="welcome-text">
+            <p>Welcome to Expense Tracker</p>
+            <button onClick={handleClick}>Let's get started!</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
