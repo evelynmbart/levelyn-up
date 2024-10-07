8b0baa3c730c7b993e9076e1c267e3bd2b3717e0
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-10-07 11:20:36 -0400

    add more html and css to the transaction form

--- 62 - Expense Tracker/expense-tracker/src/components/ExpenseTracker.jsx
+++ 62 - Expense Tracker/expense-tracker/src/components/ExpenseTracker.jsx
@@ -1,35 +1,51 @@
+import { useState } from "react";
+
 export function ExpenseTracker() {
+  const [isFormOpen, setIsFormOpen] = useState(false);
   return (
     <>
       <nav className="expense-navbar">
         <h4 className="expense-title">Expense Tracker</h4>
       </nav>
       <div className="expense-tracker-content">
-        <button className="add-btn">Add Expense/Income</button>
-        <div className="expense-form">
-          <form>
-            <div className="same-line">
-              <div className="date">
-                <label>Date</label>
-                <input type="date" />
+        <button className="add-btn" onClick={() => setIsFormOpen(!isFormOpen)}>
+          {isFormOpen ? "Close Transaction" : "Add Transaction"}
+        </button>
+        {isFormOpen && (
+          <div className="expense-form">
+            <form>
+              <div className="date-and-transaction-type">
+                <div className="date">
+                  <label>Date</label>
+                  <input type="date" />
+                </div>
+                <div className="type">
+                  <label>Expense or Income</label>
+                  <button>+</button>
+                  <button>-</button>
+                </div>
               </div>
-              <div className="type">
-                <label>Expense or Income</label>
-                <button>+</button>
-                <button>-</button>
+              <div className="amount">
+                <label>Amount $</label>
+                <input type="text" placeholder="80.00" />
               </div>
-            </div>
-            <div className="amount">
-              <label>Amount $</label>
-              <input type="text" placeholder="80.00" />
-            </div>
 
-            <div className="location">
-              <label>Spending Location</label>
-              <input type="text" placeholder="Sephora" />
-            </div>
-          </form>
-        </div>
+              <div className="location">
+                <label>Spending Location: </label>
+                <input type="text" placeholder="Key Food" />
+              </div>
+              <div className="category">
+                <label>Category: </label>
+                <input type="text" placeholder="Groceries" />
+              </div>
+              <div className="notes">
+                <label>Additional Notes: </label>
+                <textarea placeholder="Making a special occasion dinner.." />
+              </div>
+              <button className="add-transaction">Add Transaction</button>
+            </form>
+          </div>
+        )}
       </div>
     </>
   );
