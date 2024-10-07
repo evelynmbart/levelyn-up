9f6f6be022da8e58897fb6d99185da0c84f306cc
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-10-07 10:57:04 -0400

    begin expense form html & css

--- 62 - Expense Tracker/expense-tracker/src/components/ExpenseTracker.jsx
+++ 62 - Expense Tracker/expense-tracker/src/components/ExpenseTracker.jsx
@@ -1,9 +1,36 @@
 export function ExpenseTracker() {
   return (
-    <div className="expense-tracker-content">
+    <>
       <nav className="expense-navbar">
         <h4 className="expense-title">Expense Tracker</h4>
       </nav>
-    </div>
+      <div className="expense-tracker-content">
+        <button className="add-btn">Add Expense/Income</button>
+        <div className="expense-form">
+          <form>
+            <div className="same-line">
+              <div className="date">
+                <label>Date</label>
+                <input type="date" />
+              </div>
+              <div className="type">
+                <label>Expense or Income</label>
+                <button>+</button>
+                <button>-</button>
+              </div>
+            </div>
+            <div className="amount">
+              <label>Amount $</label>
+              <input type="text" placeholder="80.00" />
+            </div>
+
+            <div className="location">
+              <label>Spending Location</label>
+              <input type="text" placeholder="Sephora" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
