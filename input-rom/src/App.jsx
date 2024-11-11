7e3ef1b03d2c11686eebf263d9caff16a6099467
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-11-11 14:15:45 -0500

    add design

--- input-rom/src/App.jsx
+++ input-rom/src/App.jsx
@@ -1,7 +1,38 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="container">
+        <form>
+          <label>Full Name</label>
+          <input type="text" required placeholder="John Smith" />
+          <label>Email Address</label>
+          <input type="email" required placeholder="john.smith@smith.com" />
+          <label>Phone Number</label>
+          <input type="text" required placeholder="555-555-5555" />
+          <label>Country</label>
+          <input type="text" required placeholder="United States" />
+          <div className="location">
+            <div className="where">
+              <label id="label">State</label>
+              <input type="text" placeholder="Enter state" />
+            </div>
+            <div className="where">
+              <label id="label">City</label>
+              <input type="text" placeholder="Enter city" />
+            </div>
+            <div className="where">
+              <label id="label">Zip Code</label>
+              <input type="text" placeholder="Enter zip code" />
+            </div>
+          </div>
+
+          <button>Submit</button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default App;
