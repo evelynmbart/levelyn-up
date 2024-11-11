704eea81d9fca80668bb24499501bac61d22cdd3
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-11-11 14:27:36 -0500

    add functionality

--- input-rom/src/App.jsx
+++ input-rom/src/App.jsx
@@ -1,33 +1,106 @@
+import { useState } from "react";
 import "./App.css";
 
+const data = [
+  {
+    name: "",
+    email: "",
+    number: "",
+    country: "",
+    state: "",
+    city: "",
+    zip: "",
+  },
+];
+
 function App() {
+  const [input, setInput] = useState([]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInput((prevInput) => ({
+      ...prevInput,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = () => {
+    setInput(input);
+  };
+
+  console.log(input);
+
   return (
     <>
       <div className="container">
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>Full Name</label>
-          <input type="text" required placeholder="John Smith" />
+          <input
+            type="text"
+            required
+            placeholder="John Smith"
+            name="name"
+            value={data.name}
+            onChange={handleChange}
+          />
           <label>Email Address</label>
-          <input type="email" required placeholder="john.smith@smith.com" />
+          <input
+            type="email"
+            required
+            placeholder="john.smith@smith.com"
+            name="email"
+            value={data.email}
+            onChange={handleChange}
+          />
           <label>Phone Number</label>
-          <input type="text" required placeholder="555-555-5555" />
+          <input
+            type="text"
+            required
+            placeholder="555-555-5555"
+            name="number"
+            value={data.number}
+            onChange={handleChange}
+          />
           <label>Country</label>
-          <input type="text" required placeholder="United States" />
+          <input
+            type="text"
+            required
+            placeholder="United States"
+            name="country"
+            value={data.country}
+            onChange={handleChange}
+          />
           <div className="location">
             <div className="where">
               <label id="label">State</label>
-              <input type="text" placeholder="Enter state" />
+              <input
+                type="text"
+                placeholder="Enter state"
+                name="state"
+                value={data.state}
+                onChange={handleChange}
+              />
             </div>
             <div className="where">
               <label id="label">City</label>
-              <input type="text" placeholder="Enter city" />
+              <input
+                type="text"
+                placeholder="Enter city"
+                name="city"
+                value={data.city}
+                onChange={handleChange}
+              />
             </div>
             <div className="where">
               <label id="label">Zip Code</label>
-              <input type="text" placeholder="Enter zip code" />
+              <input
+                type="text"
+                placeholder="Enter zip code"
+                name="zip"
+                value={data.zip}
+                onChange={handleChange}
+              />
             </div>
           </div>
-
           <button>Submit</button>
         </form>
       </div>
